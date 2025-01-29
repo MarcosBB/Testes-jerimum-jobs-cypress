a14c109945e579d935b2ce0778b126f28c3bc95f
MarcosBB <marcosberaldobarros@hotmail.com>
2025-01-29 14:11:01 -0300

    testes fluxo criação de candidados

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,117 @@
 import { v4 as uuidv4 } from 'uuid'
 const { generate } = require('gerador-validador-cpf')
 
+class CandidatoCreationForm{
+  elements = {
+    inputName: () => cy.get('#nomeCandidatoInput'),
+    inputCPF: () => cy.get('#cpfInput'),
+    checkUseTerm: () => cy.get('.icheck').first(),
+    inputTelefone: () => cy.get('#telefoneCandidatoInput'),
+    inputEmail: () => cy.get('#emailCandidatoInput'),
+    inputLogin: () => cy.get('#loginCandidatoInput'),
+    submitButton: () => cy.get('button[type="submit"]')
+  }
+
+  createUser({
+    name="Teste", 
+    cpf=generate(), 
+    telefone="12345678901", 
+    email=uuidv4() + "@gmail.com", 
+    login=uuidv4().replace(/-/g, '')
+  }){
+    this.elements.inputName().type(name)
+    this.elements.inputLogin().type(login)
+    cpf !== "" && this.elements.inputCPF().type(cpf)
+    this.elements.inputEmail().type(email)
+    this.elements.inputTelefone().type(telefone)
+    this.elements.checkUseTerm().click()
+    this.elements.submitButton().click({ force: true , multiple: true})
+  }
+
+}
+
+describe('Form de criação de usuário admin', () => {
+  const userCreationForm = new CandidatoCreationForm()
+  beforeEach(() => {
+    cy.visit("/login")
+    cy.get('#btn-show-cadastro-form').click()
+  })
+
+  it('Criação de usuário admin', () => {
+    userCreationForm.createUser({})
+    cy.get('.alert-success').should('contain.text', 'Seu cadastro foi realizado com sucesso!')
+  })
+
+  it('Criação de usuário com CPF inválido', () => {
+    userCreationForm.createUser({
+      cpf: "12345678901",
+    })
+    cy.get('.alert').should('contain.text', 'Insira um CPF válido')
+  })
+
+  it('Criação de usuário com email inválido', () => {
+    userCreationForm.createUser({
+      email: "email inválido",
+    })
+    cy.get('.alert-success').should('not.exist')
+  })
+
+  it('Criação de usuário sem algum campo NECESSÁRIO', () => {
+    userCreationForm.createUser({
+      cpf: "",
+    })
+    cy.get('.alert-success').should('not.exist')
+  })
+
+  it('Criação de usuário com o mesmo login de outro', () => {
+    const login = uuidv4().replace(/-/g, '')
+    userCreationForm.createUser({
+      login: login,
+    })
+    cy.get('.alert-success').should('contain.text', 'Seu cadastro foi realizado com sucesso!')
+
+    cy.visit("/login")
+    cy.get('#btn-show-cadastro-form').click()
+    userCreationForm.createUser({
+      login: login,
+    })
+
+    cy.get('.alert-success').should('not.exist')
+  })
+
+  it('Criação de usuário com o mesmo cpf de outro', () => {
+    const cpf = generate() 
+    userCreationForm.createUser({
+      cpf: cpf,
+    })
+    cy.get('.alert-success').should('contain.text', 'Seu cadastro foi realizado com sucesso!')
+
+    cy.visit("/login")
+    cy.get('#btn-show-cadastro-form').click()
+    userCreationForm.createUser({
+      cpf: cpf,
+    })
+
+    cy.get('.alert-success').should('not.exist')
+  })
+
+  it('Criação de usuário com o mesmo email de outro', () => {
+    const email = uuidv4() + "@gmail.com"
+    userCreationForm.createUser({
+      email: email,
+    })
+    cy.get('.alert-success').should('contain.text', 'Seu cadastro foi realizado com sucesso!')
+
+    cy.visit("/login")
+    cy.get('#btn-show-cadastro-form').click()
+    userCreationForm.createUser({
+      email: email,
+    })
+
+    cy.get('.alert-success').should('not.exist')
+  })
+})
+
 class AdminCreationForm{
   elements = {
     inputName: () => cy.get('#inputNome'),
@@ -121,108 +232,3 @@ describe('Form de criação de usuário admin', () => {
     cy.get('.alert-success').should('not.exist')
   })
 })
-
-
-
-
-class CandidatoCreationForm{
-  elements = {
-    inputName: () => cy.get('#nomeCandidatoInput'),
-    inputCPF: () => cy.get('#cpfInput'),
-    checkUseTerm: () => cy.get('.iToggle-helper'),
-    inputTelefone: () => cy.get('#telefoneCandidatoInput'),
-    inputEmail: () => cy.get('#emailCandidatoInput'),
-    inputLogin: () => cy.get('#loginCandidatoInput'),
-    submitButton: () => cy.get('button[type="submit"]')
-  }
-
-  createUser({
-    name="Teste", 
-    cpf=generate(), 
-    telefone="12345678901", 
-    email=uuidv4() + "@gmail.com", 
-    login=uuidv4()
-  }){
-    this.elements.inputName().type(name)
-    this.elements.inputLogin().type(login)
-    cpf !== "" && this.elements.inputCPF().type(cpf)
-    this.elements.inputEmail().type(email)
-    this.elements.inputTelefone().type(telefone)
-    this.elements.checkUseTerm().click()
-    this.elements.submitButton().click()
-  }
-
-}
-
-// describe('Form de criação de usuário admin', () => {
-//   const userCreationForm = new CandidatoCreationForm()
-//   before(() => {
-//     cy.visit("/login#")
-//     cy.get('#btn-show-cadastro-form').click()
-//   })
-
-//   it('Criação de usuário admin', () => {
-//     userCreationForm.createUser({})
-//     cy.get('.alert-success').should('contain.text', 'Cadastrado com sucesso!')
-//   })
-
-  // it('Criação de usuário com CPF inválido', () => {
-  //   userCreationForm.createUser({
-  //     cpf: "12345678901",
-  //   })
-  //   cy.get('.alert').should('contain.text', 'Insira um CPF válido')
-  // })
-
-  // it('Criação de usuário com email inválido', () => {
-  //   userCreationForm.createUser({
-  //     email: "email inválido",
-  //   })
-  //   cy.get('.alert-success').should('not.exist')
-  // })
-
-  // it('Criação de usuário sem algum campo NECESSÁRIO', () => {
-  //   userCreationForm.createUser({
-  //     cpf: "",
-  //   })
-  //   cy.get('.alert-success').should('not.exist')
-  // })
-
-  // it('Criação de usuário com o mesmo email de outro', () => {
-  //   const email = uuidv4() + "@gmail.com"
-  //   userCreationForm.createUser({
-  //     email: email,
-  //   })
-  //   cy.get('.alert-success').should('contain.text', 'Cadastrado com sucesso!')
-  //   userCreationForm.createUser({
-  //     email: email,
-  //   })
-
-  //   cy.get('.alert-success').should('not.exist')
-  // })
-
-  // it('Criação de usuário com o mesmo login de outro', () => {
-  //   const login = uuidv4() 
-  //   userCreationForm.createUser({
-  //     login: login,
-  //   })
-  //   cy.get('.alert-success').should('contain.text', 'Cadastrado com sucesso!')
-  //   userCreationForm.createUser({
-  //     login: login,
-  //   })
-
-  //   cy.get('.alert-success').should('not.exist')
-  // })
-
-  // it('Criação de usuário com o mesmo cpf de outro', () => {
-  //   const cpf = generate() 
-  //   userCreationForm.createUser({
-  //     cpf: cpf,
-  //   })
-  //   cy.get('.alert-success').should('contain.text', 'Cadastrado com sucesso!')
-  //   userCreationForm.createUser({
-  //     cpf: cpf,
-  //   })
-
-  //   cy.get('.alert-success').should('not.exist')
-  // })
-// })
